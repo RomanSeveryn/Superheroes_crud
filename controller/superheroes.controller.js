3694d9d4d9a054f2e7c4f8209842416a87e4fee5
Roma <romaseverin50@gmail.com>
2021-04-08 20:47:28 +0300

    Development addSuperpowerToSuperheroes

--- controller/superheroes.controller.js
+++ controller/superheroes.controller.js
@@ -1,4 +1,4 @@
-const { Superheroes } = require('../models');
+const { Superheroes, Superpowers } = require('../models');
 
 module.exports.createSuperHeroes = async (req, res, next) => {
   try {
@@ -34,6 +34,36 @@ module.exports.deleteSuperHeroes = async (req, res, next) => {
     await Superheroes.destroy({ where: { id } });
 
     res.status(201).send({});
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.addSuperpowertoSuperheroes = async (req, res, next) => {
+  try {
+    const {
+      params: { superpowerId },
+      body: { superheroesId },
+    } = req;
+
+    const superheroes = await Superheroes.findByPk(superheroesId);
+    const superpowers = await Superpowers.findByPk(superpowerId);
+
+    await superpowers.addSuperheroes(superheroes);
+
+    const superheroesWithSuperpower = await Group.findAll({
+      where: { id: superpowerId },
+      include: [
+        {
+          model: Superheroes,
+          through: {
+            attributes: [],
+          },
+        },
+      ],
+    });
+
+    res.send(superheroesWithSuperpower);
   } catch (err) {
     next(err);
   }
